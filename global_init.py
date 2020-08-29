030d95387482a58a1329a0159f23be8cf2ba0bb0
aziznal <abodenaal@gmail.com>
2020-08-29 16:57:40 +0300

    Add first iteration of global_init script

--- global_init.py
+++ global_init.py
@@ -3,15 +3,25 @@ import os
 import stat
 
 
-paths = [
-    "./isbank-spyder",
-    "./kuveytturk-spyder",
-    "./vakifbank-spyder",
-    "./yapikredi-spyder",
-    "./ziraat-spyder"
-]
+def get_paths(current_os):
 
-current_os = platform.system()
+    current_dir = os.getcwd()
+
+    paths = [
+        "isbank-spyder",
+        "kuveytturk-spyder",
+        "vakifbank-spyder",
+        "yapikredi-spyder",
+        "ziraat-spyder"
+    ]
+
+    if current_os == 'Windows':
+        paths = [f'{current_dir}\\{path}\\init_script.py' for path in paths]
+
+    elif current_os == 'Linux':
+        paths = [f'{current_dir}/{path}/init_script.py' for path in paths]
+
+    return paths
 
 
 def add_execute_permissions(filename):
@@ -23,63 +33,39 @@ def add_execute_permissions(filename):
     os.chmod(filename, st.st_mode | stat.S_IEXEC)
 
 
-def create_exec_file(os, commands):
+def create_init_all(os, paths):
     if os == 'Windows':
-        with open('exec.bat', 'w') as exec_file:
-            [exec_file.write(line + "\n") for line in commands]
+        with open('init_all.bat', 'w') as file_:
+            for path in paths:
+                file_.write(f'call {path}\n')
 
     elif os == 'Linux':
-        with open('exec.sh', 'w') as exec_file:
-            [exec_file.write(line + "\n") for line in commands]
+        with open('init_all.sh', 'w') as file_:
+            for path in paths:
+                file_.write(f'sh -x {path}\n')
 
-        add_execute_permissions('exec.sh')
-
-
-def get_commands(os):
-    if os == 'Windows':
-
-        # TODO: set these up while in a windows os
-        # IDEA: use multithreading to launch all exec.bat files at once?
-        commands = []
-
-        for path in paths:
-            subcommands = [
-                f"cd {path}",
-                "call venv/Scripts/activate",
-                "python init_script.py",
-                "call exec.bat"
-            ]
-
-            commands.append(subcommands)
-
-    elif os == 'Linux':
-        commands = []
-
-        for path in paths:
-            pass
+        add_execute_permissions("init_all.sh")
 
 
-def init_windows():
-
-    commands = get_commands('Windows')
-
-    create_exec_file('Windows', commands)
+def init_windows(paths):
+    create_init_all('Windows', paths)
 
 
-def init_linux():
-
-    commands = get_commands('Linux')
-
-    create_exec_file('Linux', commands)
+def init_linux(paths):
+    create_init_all('Linux', paths)
 
 
 if __name__ == "__main__":
 
+    current_os = platform.system()
+
     if current_os == 'Windows':
-        init_windows()
+        paths = get_paths('Windows')
+        init_windows(paths)
 
     elif current_os == 'Linux':
-        init_linux()
+        paths = get_paths('Linux')
+        init_linux(paths)
 
     else:
         raise OSError('Unsupported OS')
